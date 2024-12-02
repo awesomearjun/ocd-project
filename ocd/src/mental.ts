34cea9da2922938c0c9e145cc0f25a81c015d4d2
awesomearjun <iamawesomearjun@gmail.com>
2024-12-01 22:02:22 -0500

    final grammar checks, and add emotions page, and add images

--- ocd/src/mental.ts
+++ ocd/src/mental.ts
@@ -1,1 +1,11 @@
 import "./mental.css";
+
+let transition: number = 1;
+let green: number = 255;
+let blue: number = 255;
+
+setInterval(() => {
+    green -= transition;
+    blue -= transition
+    document.querySelector("#special").style.color = `rgb(255, ${green}, ${blue})`;
+}, 800);
